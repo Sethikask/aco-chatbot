9f3404c1d5c90f7c4e35cf29c1ce0a85ecfb3d7f
Sethikask <71634303+Sethikask@users.noreply.github.com>
2020-11-08 10:52:38 +0400

    Created main.py

---
+++ main.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request
+from threading import Timer
+import webbrowser
+import requests
+import json
+import uuid
+ 
+app = Flask(__name__)
+
+#define app routes
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+def open_browser():
+    webbrowser.open_new('http://127.0.0.1:2000/')
+
+def serial_num():
+    var = str(uuid.uuid1(uuid.getnode(),0))[24:]
+    print(var)
+    return var
+
+@app.route("/get")
+#function for the bot response
+def get_bot_response():
+    userText = request.args.get('msg')
+    answer = give_answer(userText)
+    return str(answer)
+
+def give_answer(givenText):
+    uid = serial_num()
+    url = "http://api.brainshop.ai/get?bid=<MY-BID>&key=<MY-KEY>&uid=" + uid + "&msg=" + givenText
+    response = requests.get(url)
+    parsed = json.loads(response.text)['cnt']
+    return parsed
+
+if __name__ == "__main__":
+    print(serial_num())
+    Timer(1, open_browser).start();
+    app.run(port=2000)
